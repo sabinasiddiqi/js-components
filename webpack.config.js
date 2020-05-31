6d8714e73269d11a25bf84e992a77aa32e281ba0
sabinasiddiqi <sabina921@gmail.com>
2020-05-30 18:06:54 -0700

    update configs for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' }]
-      }, {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['env']
-        }
-      }
-    ]
-  }
-}
+	entry: './index.js',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+			},
+		],
+	},
+};
